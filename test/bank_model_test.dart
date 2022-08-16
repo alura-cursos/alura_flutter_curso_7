92d7f496ea339ae306993acd6443c0759bffa665
Kako <kakomo2@gmail.com>
2022-08-16 17:39:48 -0300

    Aula 2.3

--- test/bank_model_test.dart
+++ test/bank_model_test.dart
@@ -1,9 +1,23 @@
 import 'package:estilizacao_componentes/models/bank.dart';
 import 'package:flutter_test/flutter_test.dart';
 void main(){
- test('Bank model deposit should turn points into 10', (){
-   final bank = BankModel();
-   bank.deposit(10);
-   expect(bank.points, 10);
+ group('Bank model deposit tests', () {
+   test('Bank model deposit should turn points into 10', (){
+     final bank = BankModel();
+     bank.deposit(10);
+     expect(bank.points, 10);
+   });
+   test('Bank model deposit should turn available into 10', (){
+     final bank = BankModel();
+     bank.deposit(10);
+     expect(bank.available, 10);
+   });
  });
+
+test('Bank model transfer should turn points into 10', (){
+  final bank = BankModel();
+  bank.transfer(10);
+  expect(bank.points, 10);
+});
+
 }
