dc29060eaf4cb2b51df9f1e9cdb981dddc19e3a9
Kako <kakomo2@gmail.com>
2022-09-15 15:57:09 -0300

    Aula 5.2

--- test/home_test.dart
+++ test/home_test.dart
@@ -4,8 +4,10 @@ import 'package:estilizacao_componentes/data/bank_inherited.dart';
 import 'package:estilizacao_componentes/screens/home.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
 
 void main() {
+  final MockBankHttp httpMock = MockBankHttp();
   testWidgets('My widget has a text "Spent" ', (tester) async {
     await tester.pumpWidget(MaterialApp(
       home: BankInherited(
@@ -56,5 +58,14 @@ void main() {
     await tester.pumpAndSettle();
 
     expect(find.text('\$10.0'), findsOneWidget);
+  });
+  testWidgets('Testing MockHttp dolarToReal', (tester) async {
+    when(httpMock.dolarToReal()).thenAnswer((_) async => ('5'));
+    await tester.pumpWidget(MaterialApp(
+      home: BankInherited(
+        child: Home(api: httpMock.dolarToReal(),),
+      ),
+    ));
+    verify(httpMock.dolarToReal()).called(1);
   });
 }
